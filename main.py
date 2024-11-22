a3b54f407c8a5954f51712776a0c68110308e7f4
Firza Hakim <firzahakimcrack@gmail.com>
2024-11-22 21:24:53 +0700

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -95,4 +96,5 @@ def predict_skin_type():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))  # Mengambil port dari environment variable
+    app.run(host='0.0.0.0', port=port)
